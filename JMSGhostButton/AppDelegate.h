9fbbd7248481ae20e79f37c6619646e9aaa6e4e5
james an <jamesanse@gmail.com>
2015-03-10 14:57:35 +0800

    Initial Commit

---
+++ JMSGhostButton/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  JMSGhostButton
+//
+//  Created by James An on 3/10/15.
+//  Copyright (c) 2015 Jamesan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ JMSGhostButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JMSGhostButton
+//
+//  Created by James An on 3/10/15.
+//  Copyright (c) 2015 Jamesan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
